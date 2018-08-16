69a82a93d704c557fd367766ed0603feeb0f6de4
Ayman Zeine <a.zeine.96@gmail.com>
2018-08-16 15:48:18 -0700

    Initial Commit

---
+++ Bitcoin/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Bitcoin
+//
+//  Created by Ayman Zeine on 8/16/18.
+//  Copyright © 2018 Ayman Zeine. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let kBPI = "bpi"
+    static let kUSD = "USD"
+    static let kRate = "rate_float"
+    static let kUpdated = "updated"
+    
+    static let kTime = "time"
+}
---
+++ Bitcoin/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Bitcoin
+//
+//  Created by Ayman Zeine on 8/16/18.
+//  Copyright © 2018 Ayman Zeine. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    
+    fileprivate func getBitcoinPrice() -> (DataRequest) {
+        return Alamofire.request("https://api.coindesk.com/v1/bpi/currentprice.json").responseJSON { (response) in
+            
+            if let bitcoinJSON = response.result.value {
+                let bitcoinObject:Dictionary = bitcoinJSON as! Dictionary<String, Any>
+                
+                let bpiObject:Dictionary = bitcoinObject[Constants.kBPI] as! Dictionary<String, Any>
+                let usdObject:Dictionary = bpiObject[Constants.kUSD] as! Dictionary<String, Any>
+                let rate = usdObject[Constants.kRate]
+                
+                let timeObject:Dictionary = bitcoinObject[Constants.kTime] as! Dictionary<String, Any>
+                let updatedTime = timeObject[Constants.kUpdated]
+                
+                DispatchQueue.main.async {
+                    self.priceLabel.text = "$\(rate!)"
+                    self.timeStampLbl.text = "\(updatedTime!)"
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        priceLabel.text = "..."
+        timeStampLbl.text = "..."
+        
+        getBitcoinPrice()
+        
+    }
+
+}
+
